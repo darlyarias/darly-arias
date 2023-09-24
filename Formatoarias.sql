6f03c7e3ce629ba9c7f261e100f7f7259ea5c947
darlyarias <141979464+darlyarias@users.noreply.github.com>
2023-09-23 20:36:02 -0500

    Update Formatoarias.sql

--- Formatoarias.sql
+++ Formatoarias.sql
@@ -1,3 +1,4 @@
+DELIMITER $$
 CREATE DEFINER=`root`@`localhost` FUNCTION `Contar_ciudad_exterior`(desc_departamento varchar(50)) RETURNS int
     READS SQL DATA
 BEGIN
@@ -11,4 +12,25 @@ WHERE
     desc_departamento = 'Dpto del exterior';
     
     RETURN conteo;
-END
+END$$
+DELIMITER ;
+
+
+
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `ObtenerPaisPorCiudad`(cod_ciudad INT) RETURNS varchar(255) CHARSET utf8mb4
+    READS SQL DATA
+BEGIN
+    DECLARE pais VARCHAR(255);
+    
+    SELECT cp.desc_pais
+    INTO pais
+    FROM clientes.cat_ciudad cc
+    JOIN clientes.cat_pais cp ON cc.cod_pais_region_zona = cp.cod_pais_region_zona
+    ;
+    
+    RETURN pais;
+END$$
+DELIMITER ;
+
