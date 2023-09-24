c814a211803c1ecc4a12e3725be7e3f48fe47e2b
darlyarias <141979464+darlyarias@users.noreply.github.com>
2023-09-23 22:11:49 -0500

    Update Formatoarias.sql
    Se arreglan errores del código y se ponen comentarios

--- Formatoarias.sql
+++ Formatoarias.sql
@@ -1,15 +1,17 @@
+#función para contar las ciudades que pertenecen a un departamento.
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` FUNCTION `Contar_ciudad_exterior`(desc_departamento varchar(50)) RETURNS int
+#Se define como parametro de entrada el departamento al cual queremos contar las ciudades que se encuentran registradas en la BD
+CREATE DEFINER=`root`@`localhost` FUNCTION `Contar_ciudad_por_departamento`(descripcion_departamento varchar(50)) RETURNS int
     READS SQL DATA
 BEGIN
     DECLARE conteo INT;
-    
+    #Construcciónd del query
 SELECT 
     COUNT(*)
 INTO conteo FROM
     clientes.cat_ciudad
 WHERE
-    desc_departamento = 'Dpto del exterior';
+    desc_departamento = descripcion_departamento;
     
     RETURN conteo;
 END$$
@@ -17,20 +19,19 @@ DELIMITER ;
 
 
 
-
+#Función para obtener el nombre del pais de la ciudad ingresada por medio de un join entre la tabla cat_ciudad y cat_pais
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` FUNCTION `ObtenerPaisPorCiudad`(cod_ciudad INT) RETURNS varchar(255) CHARSET utf8mb4
+CREATE DEFINER=`root`@`localhost` FUNCTION `ObtenerPaisPorCiudad`(descripcion_ciudad VARCHAR(30)) RETURNS varchar(255) CHARSET utf8mb4
     READS SQL DATA
 BEGIN
-    DECLARE pais VARCHAR(255);
-    
-    SELECT cp.desc_pais
-    INTO pais
-    FROM clientes.cat_ciudad cc
-    JOIN clientes.cat_pais cp ON cc.cod_pais_region_zona = cp.cod_pais_region_zona
-    ;
-    
-    RETURN pais;
+DECLARE pais VARCHAR(255);
+
+   select p.desc_pais INTO pais
+   from clientes.cat_ciudad c
+   join clientes.cat_pais p ON c.cod_pais_region_zona = p.cod_pais_region_zona where c.desc_ciudad = descripcion_ciudad;
+#el valor que permite crear la relación en ambas tablas es cod_pais_region_zona
+RETURN pais;
+
 END$$
 DELIMITER ;
 
